950d494409c6898cecd812b2a118f339f3ffef9f
hydralw <lyplwz@126.com>
2019-08-14 14:57:39 +0800

    ces

---
+++ Lesson-one/Variable.lua
@@ -0,0 +1,18 @@
+-- 申明类型:
+local a, b
+
+-- 赋值
+a = 10
+b = 30
+
+print("value of a:", a)
+print("value of b:", b)
+
+-- 交换数据
+b, a = a, b --左值右值,a的值赋给b，b的值赋给a
+
+print("value of a:", a)
+print("value of b:", b)
+
+f = 70.0/3.0
+print("value of f:", f)
