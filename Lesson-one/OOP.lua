8ae77edf9194a7d67230878d81e7cbb32e4296aa
hydralw <lyplwz@126.com>
2019-08-25 09:55:17 +0800

    ces

---
+++ Lesson-one/OOP.lua
@@ -0,0 +1,50 @@
+-- 复制表的方式实现面向对象
+function clone(tab)
+  local ins = {}
+  for key, var in pairs(tab) do
+    ins[key] = var
+  end
+  return ins
+end          -- 克隆，以创建实例
+
+People = {}  -- 创建People类
+function People.sayHi(self)
+  print(self.name.." say hi")
+end
+
+-- 构造方法
+People.new = function(name)
+  local self = clone(People)
+  self.name = name
+
+  return self
+end
+
+-- 创建实例
+local p = People.new("ZhangSan")
+-- p.sayHi(p)
+p:sayHi()
+
+-- 类的继承
+function copy(dist,tab)
+  for key, var in pairs(tab) do
+    dist[key] = var
+  end
+end  -- 复制
+
+Man = {}
+Man.new = function (name)
+  local self = People.new(name)
+  copy(self, Man)
+  return self
+end
+
+-- 创建新的方法
+function Man.sayHello()
+  print("Man say hello")
+end
+
+-- 创建实例
+local p = Man.new("LiSi")
+p:sayHello()
+p:sayHi() -- 继承父类的方法
